e8fcbc5caad0c62c0fc5d3f2897c8fd259aef958
mrbhonehtet-cmd <mr.bhonehtet@gmail.com>
2025-09-04 15:01:03 +0630

    server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(cors({
     'http://localhost:8000', 
     'http://127.0.0.1:5500',
     'http://127.0.0.1:8000',
-    'https://your-netlify-app.netlify.app'
+    'https://dulcet-otter-bfa1e4.netlify.app'
   ], // Local development + Netlify URL
   credentials: true
 }));
